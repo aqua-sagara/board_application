99a9fb6a65f1ec7138a30f318492dce2af0fbfd8
aqua-sagara <aqua.ns.0913@outlook.jp>
2017-11-20 10:36:58 +0900

    Add files via upload

--- home.php
+++ home.php
@@ -59,7 +59,11 @@ catch (PDOException $e) {
 
                 <div class="col-xl-3">
 
-        <form method="post">
+
+            <form enctype="multipart/form-data" method="post" action="">
+                <fieldset>
+                    <legend>画像ファイルを選択(GIF, JPEG, PNGのみ対応)</legend>
+                    <input type="file" name="upfile" /><br />
           <div class=".card">
         title:
         <textarea name="title" rows="2" cols="35"></textarea><br></div>
@@ -67,8 +71,42 @@ catch (PDOException $e) {
         text:
         <textarea name="text" rows="8" cols="35"></textarea><br>
         <input type="submit" value="投稿" name="text_sub">
+
       </div>
-    </form></div>
+    </fieldset></form>
+                    <?php if (!empty($msgs)): ?>
+                        <fieldset>
+                            <legend>メッセージ</legend>
+                            <?php foreach ($msgs as $msg): ?>
+                                <ul>
+                                    <li style="color:<?=h($msg[0])?>;"><?=h($msg[1])?></li>
+                                </ul>
+                            <?php endforeach; ?>
+                        </fieldset>
+                    <?php endif; ?>
+                    <?php if (!empty($rows)): ?>
+                        <fieldset>
+                            <legend>サムネイル一覧(クリックすると原寸大表示)</legend>
+                            <?php foreach ($rows as $i => $row): ?>
+                                <?php if ($i): ?>
+                                    <hr />
+                                <?php endif; ?>
+                                <p>
+                                    <?=sprintf(
+                                        '<a href="?id=%d"><img src="data:%s;base64,%s" alt="%s" /></a>',
+                                        $row['id'],
+                                        image_type_to_mime_type($row['type']),
+                                        base64_encode($row['thumb_data']),
+                                        h($row['name'])
+                                    )?><br />
+                                    ファイル名: <?=h($row['name'])?><br />
+                                    日付: <?=h($row['date'])?><br clear="all" />
+                                </p>
+                            <?php endforeach; ?>
+                        </fieldset>
+                    <?php endif; ?>
+
+                </div>
         <!--<div  class="container">-->
          <div  class="col-xl-9 ">
 
@@ -93,6 +131,8 @@ catch (PDOException $e) {
           //       $errorMessage = 'パスワードが未入力です。';
             // }
         if (!empty($_POST["title"]) && !empty($_POST["text"]) ) {
+            /* アップロードがあったとき */
+
           date_default_timezone_set('Asia/Tokyo');
             $format=date("Y-m-d H:i:s");
             try{
@@ -105,9 +145,83 @@ catch (PDOException $e) {
               $prepare->bindValue(':text',$_POST['text']);
               $prepare->bindValue(':title',$_POST['title']);
               $prepare->execute();
+                $autoincrement = $pdo->lastInsertId();
             }
             catch (PDOException $e) {
                 $errorMessage = 'データベースエラー';
+            }
+            if (isset($_FILES['upfile']['error']) && is_int($_FILES['upfile']['error'])&&$_FILES['upfile']['size']!=0) {
+
+                // バッファリングを開始
+                ob_start();
+
+                try {
+
+                    // $_FILES['upfile']['error'] の値を確認
+                    switch ($_FILES['upfile']['error']) {
+                        case UPLOAD_ERR_OK: // OK
+                            break;
+
+                        case UPLOAD_ERR_INI_SIZE:  // php.ini定義の最大サイズ超過
+                        case UPLOAD_ERR_FORM_SIZE: // フォーム定義の最大サイズ超過
+                            throw new RuntimeException('ファイルサイズが大きすぎます', 400);
+                        default:
+                            throw new RuntimeException('その他のエラーが発生しました', 500);
+                    }
+
+                    // $_FILES['upfile']['mime']の値はブラウザ側で偽装可能なので
+                    // MIMEタイプを自前でチェックする
+                    if (!$info = @getimagesize($_FILES['upfile']['tmp_name'])) {
+                        throw new RuntimeException('有効な画像ファイルを指定してください', 400);
+                    }
+                    if (!in_array($info[2], [IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG], true)) {
+                        throw new RuntimeException('未対応の画像形式です', 400);
+                    }
+
+                    // サムネイルをバッファに出力
+                    $create = str_replace('/', 'createfrom', $info['mime']);
+                    $output = str_replace('/', '', $info['mime']);
+                    if ($info[0] >= $info[1]) {
+                        $dst_w = 120;
+                        $dst_h = ceil(120 * $info[1] / max($info[0], 1));
+                    } else {
+                        $dst_w = ceil(120 * $info[0] / max($info[1], 1));
+                        $dst_h = 120;
+                    }
+                    if (!$src = @$create($_FILES['upfile']['tmp_name'])) {
+                        throw new RuntimeException('画像リソースの生成に失敗しました', 500);
+                    }
+                    $dst = imagecreatetruecolor($dst_w, $dst_h);
+                    imagecopyresampled($dst, $src, 0, 0, 0, 0, $dst_w, $dst_h, $info[0], $info[1]);
+                    $output($dst);
+                    imagedestroy($src);
+                    imagedestroy($dst);
+
+                    // INSERT処理
+                    $stmt = $pdo->prepare('INSERT INTO image(id,name,type,raw_data,thumb_data,date) VALUES(?,?,?,?,?,?)');
+                    //$stmt->bindValue(':id',$autoincrement);
+                    $stmt->execute([
+                            $autoincrement,
+                        $_FILES['upfile']['name'],
+                        $info[2],
+                        file_get_contents($_FILES['upfile']['tmp_name']),
+                        ob_get_clean(), // バッファからデータを取得してクリア
+                        (new DateTime('now', new DateTimeZone('Asia/Tokyo')))->format('Y-m-d H:i:s'),
+                    ]);
+
+                    $msgs[] = ['green', 'ファイルは正常にアップロードされました'];
+
+                } catch (RuntimeException $e) {
+
+                    while (ob_get_level()) {
+                        ob_end_clean(); // バッファをクリア
+                    }
+                    http_response_code($e instanceof PDOException ? 500 : $e->getCode());
+                    $msgs[] = ['red', $e->getMessage()];
+
+                }
+
+                /* ID指定があったとき */
             }
           }
         }
@@ -167,6 +281,24 @@ catch (PDOException $e) {
 
 
               echo "</td></div><td class=\"td1\"><div class=\"col-xl-9\">";
+
+                try {
+
+                    $stmt = $pdo->prepare('SELECT type, raw_data FROM image WHERE id = :id ');
+                    $stmt->bindValue(':id',$board[$count]['board_id']);
+                    $stmt->execute();
+                    if (!$row = $stmt->fetch()) {
+                        throw new RuntimeException('該当する画像は存在しません', 404);
+                    }
+                    //header('X-Content-Type-Options: nosniff');
+                   // header('Content-Type: ' . image_type_to_mime_type($row['type']));
+                    //echo $row['raw_data'];
+                } catch (RuntimeException $e) {
+
+                    //http_response_code($e instanceof PDOException ? 500 : $e->getCode());
+                    //$msgs[] = ['red', $e->getMessage()];
+
+                }
               echo nl2br(htmlspecialchars($board[$count]['text'],ENT_QUOTES))."　　　　　　　　　　　　　";
 
               if($board[$count]['user_id']==$id){
@@ -206,11 +338,13 @@ if(isset($_POST['delete'])) {
   }
 }
 
+
 //ページ切り替え
 if(isset($_POST['page'])){
   $page=$_post['page_num'];
   header("Locating:home.php");
 }
         ?>
+
     </body>
 </html>
